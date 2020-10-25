f1b9813eae082f8fa2e25ee1f9d0bd63748b4287
zeph <namastebackend@gmail.com>
2020-10-25 22:33:15 +0545

    firebase signup signin with navbar btns

---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes';
+import SignOutButton from "../SignOut";
+
+const Navigation = ({authUser}) => authUser ? <AuthNavigation/> : <NonAuthNavigation/>
+
+const AuthNavigation = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to={ROUTES.LANDING}>Landing</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.HOME}>Home</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.ACCOUNT}>Account</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.ADMIN}>Admin</Link>
+                </li>
+                <li>
+                    <SignOutButton/>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+const NonAuthNavigation = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.LANDING}>Landing</Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navigation;
